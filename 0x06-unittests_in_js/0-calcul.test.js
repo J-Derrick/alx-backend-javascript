ddc255dea8ccd111dcb6830fd051e109c26defde
J-Derrick <j.derrickwaweru@gmail.com>
2024-05-03 03:36:48 +0300

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,24 +1,21 @@
 const assert = require('assert');
-const { it, describe } = require('mocha');
-const calculateNumber = require('./1-calcul');
+const {it, describe} = require('mocha');
+const calculateNumber = require('./0-calcul')
 
 describe('calculateNumber', () => {
-    it('checking if operation is correct 1', () => {
-        assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+    it('checking if number round', () => {
+        assert.equal(calculateNumber(1, 3), 4);
     });
-    it('checking if operation is correct 2', () => {
-        assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    it('chacking if numbers round 2nd round', () => {
+        assert.equal(calculateNumber(1, 3.7), 5);
     });
-    it('checking if operation is correct 3', () => {
-        assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    it('checking if numbers round 3rd round', () => {
+        assert.equal(calculateNumber(1.2, 3.7), 5);
     });
-    it('checking if operation is correct 4', () => {
-        assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    it('checking if numbers round 4th round', () => {
+        assert.equal(calculateNumber(1.5, 3.7), 6);
     });
-    it('checking correct type for operation 1', () => {
-        assert.equal(calculateNumber(5, 1, 4), 'Error');
-    });
-    it('checking correct type for operation 2', () => {
-        assert.equal(calculateNumber('plus', 1, 4), 'Error');
+    it('chacking negative return', () => {
+        assert.equal(calculateNumber(-1.3, -3.7), -5);
     });
 });
